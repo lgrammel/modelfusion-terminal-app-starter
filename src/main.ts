7fad9d510610fe9f337c52f2c5d758a9d9286745
Lars Grammel <lars.grammel@gmail.com>
2023-09-25 20:39:43 +0200

    Update version. Use turbo-instruct.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import { OpenAIChatMessage, OpenAIChatModel, streamText } from "modelfusion";
 import dotenv from "dotenv";
+import { OpenAITextGenerationModel, streamText } from "modelfusion";
 
 dotenv.config();
 
 (async () => {
   const textStream = await streamText(
-    new OpenAIChatModel({ model: "gpt-3.5-turbo", maxCompletionTokens: 500 }),
-    [
-      OpenAIChatMessage.system(
-        "Say 'Hello, World!' in 25 different languages without mentioning the language name."
-      ),
-    ]
+    new OpenAITextGenerationModel({
+      model: "gpt-3.5-turbo-instruct",
+      maxCompletionTokens: 500,
+    }),
+    "Say 'Hello, World!' in 25 different languages without mentioning the language name."
   );
 
   for await (const textFragment of textStream) {
